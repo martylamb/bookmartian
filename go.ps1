3bb4036a13f02f3526c76be35bc0ecdc9f4b1f8c
John Mutchek <John Mutchek>
2016-09-08 20:40:25 -0400

    adjust scripts for backend changes

--- go.ps1
+++ go.ps1
@@ -1,3 +1,3 @@
 # powershell script to launch the bookmartian server
 $env:BOOM_DEBUG=1
-mvn "exec:java" "-Dexec.mainClass=com.martiansoftware.bookmartian.App" "-Dexec.classpathScope=compile" "-Dexec.args=bookmarks.json"
+mvn "exec:java" "-Dexec.mainClass=com.martiansoftware.bookmartian.App" "-Dexec.classpathScope=compile" "-Dexec.args=-d ."
