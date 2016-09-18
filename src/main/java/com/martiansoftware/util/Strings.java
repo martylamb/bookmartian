875cc5c0d59ce079867a4cf7965f19b0f0728eec
Marty Lamb <mlamb@martiansoftware.com>
2016-09-17 20:43:55 -0400

    adding comments

--- src/main/java/com/martiansoftware/util/Strings.java
+++ src/main/java/com/martiansoftware/util/Strings.java
@@ -10,33 +10,65 @@ import java.util.Locale;
  */
 public class Strings {
     
+    /**
+     * Returns true if String is either null or zero-length.
+     * @param s the String to test
+     * @return true if String is either null or zero-length.
+     */
     public static boolean isEmpty(String s) {
         if (s == null) return true;
         return (s.isEmpty());
     }
     
+    /**
+     * Trims a String, converting nulls to empty Strings.
+     * @param s the String to trim
+     * @return the trimmed, not-null String
+     */
     public static String safeTrim(String s) {
         return (s == null) ? "" : s.trim();
     }
     
+    /**
+     * Trims a String, converting empty strings to null
+     * @param s the String to trim
+     * @return the trimmed, not-empty but possibly null String
+     */
     public static String safeTrimToNull(String s) {
         String result  = safeTrim(s);
         return result.isEmpty() ? null : result;
     }
     
+    /**
+     * Lowercases a String using the root Locale
+     * @param s the String to lowercase
+     * @return the lowercased String
+     */
     public static String lower(String s) {
         if (s == null) return null;
         return s.toLowerCase(Locale.ROOT);
     }
         
+    /**
+     * Parses a String into a long, possibly throwing a RuntimeException
+     * @param s the String to parse into a long
+     * @return the parsed long
+     */
     public static long asLong(String s) {
         try {
             return Long.valueOf(s);
         } catch (Exception e) {
-            throw new IllegalArgumentException(String.format("not a number: '%s'", s));
+            return Oops.oops("not a number: '%s'", s);
         }
     }
         
+    /**
+     * Given a String, splits it around any whitespace or commas and converts
+     * it into a List with no empty Strings
+     * @param toSplit the String to split
+     * @return a List of nonempty Strings that were separated by whitespace
+     * or commas
+     */
     public static List<String> splitOnWhitespaceAndCommas(String toSplit) {
         List<String> result = new java.util.ArrayList<>();
         if (toSplit != null) {
