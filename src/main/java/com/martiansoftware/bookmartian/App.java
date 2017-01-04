martylamb/bookmartian

commit 003e8fa7a60568a5c81ace212e61451316fd65e4

close #41 - instead of redirecting / to index.html, I'm redirecting index.html to / and returning the contents of static-content/index.html.  Result is a cleaner address bar. :)  No caching of content (index.html is read from the classpath on every request) but not planning to implement that unless performance turns out to be an issue.

src/main/java/com/martiansoftware/bookmartian/App.java
@@ -26,6 +26,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -35,6 +36,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
+import spark.utils.IOUtils;
 
 /**
  *
@@ -87,6 +89,9 @@ public class App {
         post("/api/bookmark/delete", () -> deleteBookmark(bm));
         
         post("/api/bookmarks/import", () -> importNetscapeBookmarksFile(bm));
+        
+        get("/", (req, rsp) -> IOUtils.toString(App.class.getResourceAsStream("/static-content/index.html")));
+        get("/index.html", (req, rsp) -> { rsp.redirect("/", HttpServletResponse.SC_MOVED_PERMANENTLY); return null; });
     }
     
     private static BoomResponse query(IBookmartian bm) {

commit da7c0f3ecc0be99eecd594da5a162ec67b249d10

fix issue caused by 003e8fa7a60568a5c81ace212e61451316fd65e4 in which redirect would drop query string (thus dropping pins)

src/main/java/com/martiansoftware/bookmartian/App.java
@@ -27,6 +27,7 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.http.HttpServletResponse;
@@ -98,7 +99,11 @@ public class App {
         post("/api/bookmarks/import", () -> importNetscapeBookmarksFile(bm));
         
         get("/", (req, rsp) -> IOUtils.toString(App.class.getResourceAsStream("/static-content/index.html")));
-        get("/index.html", (req, rsp) -> { rsp.redirect("/", HttpServletResponse.SC_MOVED_PERMANENTLY); return null; });
+        get("/index.html", (req, rsp) -> {             
+            String q = req.raw().getQueryString();
+            String dest = String.format("/%s%s", Strings.isEmpty(q) ? "" : "?", Strings.isEmpty(q) ? "" : q);
+            rsp.redirect(dest, HttpServletResponse.SC_MOVED_PERMANENTLY); return null; 
+        });
     }
     
     private static BoomResponse query(IBookmartian bm) {