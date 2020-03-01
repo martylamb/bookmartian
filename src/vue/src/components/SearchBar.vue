9cbe07c300eb0f5254e57310cb50fdf1d9185f6c
jmutchek <john5@mutchek.com>
2020-03-01 08:56:30 -0500

    searchbar

---
+++ src/vue/src/components/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="search">
+    <p>
+      <input type="text" :value="query" v-focus/>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchBar',
+  props: {
+    // pre-filled query can be sent to the component and defaults into the search bar
+    query: String
+  },
+  directives: {
+    // directive definition to set focus on a componenet (used on page load for the search bar)
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+a {
+  color: #42b983;
+}
+
+</style>
--- src/vue/src/views/Home.vue
+++ src/vue/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchBar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from '@/components/SearchBar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
