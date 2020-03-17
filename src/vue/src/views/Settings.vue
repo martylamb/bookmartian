67614d21c0e3cea4335dc2f5cab759868078b985
jmutchek <john5@mutchek.com>
2020-03-17 17:33:29 -0400

    color change

--- src/vue/src/views/Settings.vue
+++ src/vue/src/views/Settings.vue
@@ -2,10 +2,10 @@
   <div class='page'>
     <h1>Configuration</h1>
     <p>URL to an external configuration file. The format of the file is described here, and the json schema is available here.</p>
-    
+
     <h1>Bookmarklet</h1>
     <p>Using <a :href='this.bookmarkFQDN'>this bookmarklet</a> is the easiest way to add bookmarks to the bookmartian.</p>
-    
+
     <h1>Manage your tags</h1>
     <p>Rename, merge and recolor tags used in your bookmark collection.</p>
     <div class='tagactions'>
@@ -20,16 +20,16 @@
         </b-select>
 
         <span class='tag' :style='{ "background-color": tagBGColor(currentTag), color: tagFGColor(currentTag) }'>{{ currentTag }}</span>
-        
+
         <b-field >
           <p class='control'>
             <b-input :disabled='currentTag.length==0' placeholder='#ffffff' style='width:100px' v-model='tagColor'></b-input>
           </p>
           <p class='control'>
-            <b-button :disabled='currentTag.length==0' class='button is-info'>set color</b-button>
+            <b-button :disabled='currentTag.length==0' class='button is-info' v-on:click='updateTag()'>set color</b-button>
           </p>
         </b-field>
-        
+
         <b-field >
           <b-input :disabled='currentTag.length==0' placeholder='new tagname' style='width:120px' ></b-input>
           <p class='control'>
@@ -41,7 +41,7 @@
 
     <h1>Import bookmarks from a file</h1>
     <p>You can import bookmarks into Bookmartian from an existing file. The file must be formatted in the Netscape Bookmark file format.</p>
-    
+
     <h1>About this project</h1>
     <p>Bookmartian is a self-hosted, personal bookmark database intended to make saving, finding, and using bookmarks simple but powerful. Designed to be easily hosted on your home machine or on a cloud server, bookmartian is self-contained with no external software dependencies. Enjoy! Marty Lamb &amp; John Mutchek</p>
     <p class="monospace aboutbox">
@@ -135,7 +135,7 @@ export default {
     },
     // set default tag color from cache if none specified
     tagBGColor: function (tag) {
-      var color = this.tagCache.get(tag)
+      var color = this.tagColor
       if (color === '#000000') {
         color = '#ffffff'
       }
@@ -143,12 +143,35 @@ export default {
     },
     // set default tag color from cache if none specified
     tagFGColor: function (tag) {
-      var color = this.tagCache.get(tag)
+      var color = this.tagColor
       if (color !== '#000000') {
         color = '#ffffff'
       }
       return color
-    },    
+    },
+    updateTag: function () {
+      console.log('Settings: saving tag ' + this.currentTag)
+      const axios = require('axios')
+      const qs = require('qs')
+      axios
+        .post('/api/tag/update',
+          qs.stringify({
+            name: this.currentTag,
+            color: this.tagColor
+          }))
+        .then(response => {
+          // handle success
+          this.tagCache.set(this.currentTag, this.tagColor)
+          console.log('Settings: successful save')
+        })
+        .catch(error => {
+          // handle error
+          console.log(error)
+        })
+        .finally(function () {
+          // always executed
+        })
+    }
   },
   mounted () {
     // blank out the searchbar whenever a dashboard page is loaded
