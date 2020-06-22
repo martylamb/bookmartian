1b90d51417a7bd763dee48ab038851c7fcb232f2
Marty Lamb <mlamb@martiansoftware.com>
2020-06-22 09:23:32 -0400

    (temporary) log default charset during startup

--- src/main/java/com/martiansoftware/bookmartian/App.java
+++ src/main/java/com/martiansoftware/bookmartian/App.java
@@ -25,6 +25,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -99,7 +100,9 @@ public class App {
             appProperties.getProperty("git.commit.id.describe-short"));
         
         System.out.format("%s%n%n", s);
-        log.info(s);        
+        log.info(s);
+        
+        log.info("Default charset={}", Charset.defaultCharset());
     }
     
     public static void main(String[] args) throws Exception {
